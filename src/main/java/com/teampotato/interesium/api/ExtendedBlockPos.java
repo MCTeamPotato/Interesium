6ba9f8444bfcd96853762358003e6e9b44a4670d
Kasualix <670067575@qq.com>
2023-12-08 20:14:55 +0800

    hmmm, that's not worth

--- src/main/java/com/teampotato/interesium/api/ExtendedBlockPos.java
+++
@@ -1,8 +0,0 @@
-package com.teampotato.interesium.api;
-
-import org.jetbrains.annotations.Nullable;
-
-public interface ExtendedBlockPos {
-    @Nullable Boolean interesium$isInBlacklistedTargets();
-    void interesium$setIsInBlacklistedTargets(@Nullable Boolean isInBlacklistedTargets);
-}
